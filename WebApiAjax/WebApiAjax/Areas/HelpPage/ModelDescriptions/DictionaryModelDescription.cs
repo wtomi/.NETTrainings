c031fa6a7ee9a7d12833b54a604f0b3a3972d6a9
tomi <tomi.git@gmail.com>
2018-06-03 22:39:23 +0200

    Add ajax form handling

---
+++ WebApiAjax/WebApiAjax/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebApiAjax.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ WebApiAjax/WebApiAjax/Controllers/MathController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiAjax.Models;
+
+namespace WebApiAjax.Controllers
+{
+    public class MathController : ApiController
+    {
+        // GET api/math/calculate
+        [HttpGet]
+        public CalcResult Calculate(int x, int y)
+        {
+            return new CalcResult()
+            {
+                Sum = x + y,
+                Difference = x - y,
+                Product = x * y,
+                Quotient = x / y
+            };
+        }
+    }
+}
